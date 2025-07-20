529c8a2f4ba8bb39ffe8f1846a85596d81825bdc
Trocedge <Xfan0202@163.com>
2025-07-20 10:48:40 +0800

    Modify the path mapping and add the C locale configuration

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'HAUST计算机类学生技能引导文档',
-  base: "/ceasso-skill-docs/",
+  base: "/", //If use GitHub Pages, set base to '/repo-name/', such as '/ceasso-skill-docs/'
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
@@ -18,5 +18,7 @@ export default defineConfig({
         content: 'https://github.com/web-infra-dev/rspress',
       },
     ],
+    prevPageText: '上一页',
+    nextPageText: '下一页',
   },
 });
